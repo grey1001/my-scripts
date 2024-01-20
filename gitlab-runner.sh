0e1c566f0132c6e74ece819a9df11bd0f500231e
Grey <thegrey@rocketmail.com>
2024-01-20 11:48:10 +0100

    added unzip

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -49,5 +49,6 @@ sudo gitlab-runner start
 
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install -y
